4807702f16239c432213db47e5c45be9e99a9e83
lirantul123 <93045864+lirantul123@users.noreply.github.com>
2022-06-02 21:38:31 +0300

    update

--- first.java
+++ first.java
@@ -1,19 +1,21 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class first{    
     public static Scanner in = new Scanner(System.in);
 
     private static String text = "";
+    
     public static void main(String[] args) {
         while(text!= "stop"){
             System.out.print("C:> ");
             text = in.nextLine();
 
             if (text.substring(text.charAt(0), text.charAt(3)) == "make"
-                    && text.substring(text.charAt(4), text.charAt(4)) == " "){
+                    && text.substring(text.charAt(4), text.charAt(4)) == " ")
+                
                 System.out.println(text.substring(text.charAt(5), text.charAt(text.length()-1)));
 
-            }else
+            else
                 continue;
         }
         System.out.println("!Program Stopped!");
