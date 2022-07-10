fd6b53ac8739e257b7504501c6377fe5f4f9fb26
lirantul123 <lira.tulchin@gmail.com>
2022-07-10 19:32:43 +0300

    more Updates

--- e.py
+++ e.py
@@ -14,8 +14,34 @@ def add():
     pwd = input("Password: ")
 
     with open('passwords.txt', 'a') as f:
-        f.write(name + "|" + pwd + "\n")
+        f.write(name + " | " + pwd + "\n")
 
+def game():
+    count=0
+    a = input("What is RAM: ".lower())
+    if a == "random access memory":
+        count+=1
+    else:
+        pass
+
+    b = input("What is 1+1: ".lower())
+    if b == "2":
+        count+=1
+    else:
+        pass
+
+    c = input("What is 2+2: ".lower())
+    if c == "4":
+        count+=1
+    else:
+        pass
+
+    d = input("What is 3+3: ".lower())
+    if d == "6":
+        count+=1
+    else:
+        pass
+    print ("You got ", (count/4)*100, "%.")
 
 
 while True:
@@ -24,6 +50,8 @@ while True:
     if mode == "q":
         break
     
+    if mode == "game":
+        game()
     if mode == "view":
         view()
     elif mode == "add":
